21565b6da0acf993289dc1d771b5a0037981821f
yo-less <jens.olbertz@gmail.com>
2016-09-02 00:37:23 +0200

    node helper log message unified & request version dependency fixed

--- node_helper.js
+++ node_helper.js
@@ -14,7 +14,7 @@ const NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 
     start: function() {
-        console.log("Starting modules: " + this.name);
+        console.log("Starting node helper for: " + this.name);
     },
 
 	
