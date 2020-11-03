bb4e21f41b3b51eb9e48a944bfa3e72eead28c89
naveda89 <alvaro@naveda.me>
2020-11-03 23:17:20 +0100

    Several improvements (multiple stations per module, different layout, list instead of table, etc...)

--- MMM-nextbike.js
+++ MMM-nextbike.js
@@ -8,13 +8,12 @@
 Module.register("MMM-nextbike", {
   defaults: {
     apiBase: "http://api.nextbike.net/maps/nextbike-official.xml",
-    cityID: 1,
-    stationID: 16337,
-    stationName: "nextbike",
+    cityId: "1",
+    stations: [{ id: "16337", name: "nextbike" }],
     showBikes: true,
     nob: "",
     reload: 1 * 60 * 1000, // every minute
-    showHeader: true,
+    showHeader: true
   },
 
   getTranslations: function () {
@@ -39,19 +38,17 @@ Module.register("MMM-nextbike", {
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "BIKES" + this.config.stationID) {
-      this.nextbikeData = payload.markers.country[0].city[0].place[0].$;
-      this.config.stationName = this.nextbikeData.name;
+    if (notification === "BIKES" + this.config.cityId) {
+      this.nextbikeData = payload;
       this.updateDom();
     }
   },
 
   getDom: function () {
     // Auto-create MagicMirror header
-
     var wrapper = document.createElement("div");
 
-    if (this.config.showHeader) {
+    if (!!this.config.showHeader) {
       var header = document.createElement("header");
       if (this.config.stationName !== "nextbike") {
         header.innerHTML = this.config.stationName;
@@ -62,93 +59,45 @@ Module.register("MMM-nextbike", {
     }
 
     // Loading data notification
-
     if (!this.nextbikeData) {
       var text = document.createElement("div");
       text.innerHTML = this.translate("LOADING");
       text.className = "small dimmed";
       wrapper.appendChild(text);
     } else {
-      // Create bike table once data is received
+      // Create stations list
+      var stationsList = document.createElement("ul");
+      stationsList.className = "small stationsList";
+      
+      this.nextbikeData.forEach(station => {
+        Log.info(station);
+        var stationConfig = this.getStationConfig(station);
+        var stationElement = document.createElement("li");
+        stationElement.className = "stationElement";
 
-      var table = document.createElement("table");
-      table.classList.add("small", "table");
-      table.border = "0";
-      table.appendChild(this.createSpacerRow());
-      table.appendChild(this.createAmountRow());
-      table.appendChild(this.createSpacerRow());
+        var stationName = document.createElement("span")
+        stationName.className = "stationLabel dimmed";
+        stationName.innerHTML = `<span class="stationName">${stationConfig.name}</span> <i class="fa fa-bicycle"></i> `;
+        stationElement.appendChild(stationName);
 
-      // List available bikes via a bike array
-
-      if (this.config.showBikes) {
-        // Make sure user wants to see the bikes
-
-        if (!this.nextbikeData.bike_numbers) {
-          this.hide(10000);
+        var stationInfo = document.createElement("span");
+        stationInfo.className = "stationInfo";
+        if (!station.$.bike_numbers) {
+          stationInfo.innerHTML = this.translate("NO-BIKES-AVAILABLE");
         } else {
-          if (this.hidden) {
-            this.show(5000);
-          }
-
-          var bikeArray = this.nextbikeData.bike_numbers.split(",");
-          if (!this.config.nob) {
-            this.config.nob = 100;
-          }
-          for (var i = 0; i < bikeArray.length && i < this.config.nob; i++) {
-            var bikeNumber = bikeArray[i];
-            table.appendChild(this.createDataRow(bikeNumber));
-          }
+          stationInfo.innerHTML = `<span class="available-bikes">${station.$.bikes}</span> / <span class="total-bikes">${station.$.bike_racks}</span>`;
         }
-      }
+        stationElement.appendChild(stationInfo);
+        stationsList.appendChild(stationElement);
+      });
 
-      wrapper.appendChild(table);
+      wrapper.appendChild(stationsList);
     }
 
     return wrapper;
   },
 
-  createSpacerRow: function () {
-    var spacerRow = document.createElement("tr");
-
-    var spacerHeader = document.createElement("td");
-    spacerHeader.className = "spacerRow";
-    spacerHeader.setAttribute("colSpan", "2");
-    spacerHeader.innerHTML = "";
-    spacerRow.appendChild(spacerHeader);
-
-    return spacerRow;
+  getStationConfig: function (station) {
+    return this.config.stations.find(config => config.id == station.$.uid);
   },
-
-  createAmountRow: function () {
-    var amountRow = document.createElement("tr");
-
-    var amount = document.createElement("td");
-    amount.className = "amountRow";
-    amount.setAttribute("colSpan", "2");
-    if (!this.nextbikeData.bike_numbers) {
-      amount.innerHTML = this.translate("NO-BIKES-AVAILABLE");
-    } else {
-      amount.innerHTML = this.translate("BIKES-AVAILABLE") + " " + this.nextbikeData.bikes;
-    }
-    amountRow.appendChild(amount);
-
-    return amountRow;
-  },
-
-  createDataRow: function (data) {
-    var row = document.createElement("tr");
-
-    var symbol = document.createElement("td");
-    symbol.setAttribute("width", "8px");
-    symbol.className = "fa fa-bicycle";
-    row.appendChild(symbol);
-
-    var bikeNo = document.createElement("td");
-    bikeNo.className = "bikeNo";
-    bikeNo.innerHTML = data;
-
-    row.appendChild(bikeNo);
-
-    return row;
-  }
 });
--- node_helper.js
+++ node_helper.js
@@ -5,57 +5,52 @@
  * MIT Licensed.
  */
 
-var parseString = require('xml2js').parseString;
-const request = require('request');
+var parseString = require("xml2js").parseString;
+const request = require("request");
 const NodeHelper = require("node_helper");
 
-
-
 module.exports = NodeHelper.create({
+  start: function () {
+    console.log("Starting node helper for: " + this.name);
+  },
 
-    start: function() {
-        console.log("Starting node helper for: " + this.name);
-    },
+  /* getParams
+   * Generates an url with api parameters based on the config.
+   *
+   * return String - URL params.
+   */
 
-	
-	/* getParams
-	 * Generates an url with api parameters based on the config.
-	 *
-	 * return String - URL params.
-	 */
-	
-	getParams: function() {
-			var params = "?city=";
-			params += this.config.cityID;
-			params += "&place=";
-			params += this.config.stationID;
-			return params;
-	},
-	
-    socketNotificationReceived: function(notification, payload) {
-        if(notification === 'CONFIG'){
-            this.config = payload;
-			var nextbike_url = this.config.apiBase + this.getParams();
-			this.getData(nextbike_url, this.config.stationID);
-        }
-    },
+  getParams: function () {
+    var params = "?city=";
+    params += this.config.cityId;
+    return params;
+  },
 
-	parseData: function(input) {
-				var nextbikeData = "";
-				parseString(input, function (err, result) {
-				nextbikeData = JSON.parse(JSON.stringify(result));
-				});
-				return nextbikeData;
-	},
-	
-	
-    getData: function(options, stationID) {
-		request(options, (error, response, body) => {
-	        if (response.statusCode === 200) {
-				this.sendSocketNotification("BIKES" + stationID, this.parseData(body));
-				} else {
-                console.log("Error getting nextbike data " + response.statusCode);
-            }
-        });
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "CONFIG") {
+      this.config = payload;
+      var nextbike_url = this.config.apiBase + this.getParams();
+      this.getData(nextbike_url, this.config.cityId);
     }
+  },
+
+  parseData: function (input) {
+    var nextbikeData = "";
+    parseString(input, function (err, result) {
+      nextbikeData = JSON.parse(JSON.stringify(result));
+	});
+	var stations = nextbikeData.markers.country[0].city[0].place;
+	var stationIds = this.config.stations.map(station => station.id);
+	return stations.filter(station => stationIds.includes(station.$.uid));
+  },
+
+  getData: function (options, cityId) {
+    request(options, (error, response, body) => {
+      if (response.statusCode === 200) {
+        this.sendSocketNotification("BIKES" + cityId, this.parseData(body));
+      } else {
+        console.log("Error getting nextbike data " + response.statusCode);
+      }
+    });
+  }
 });
